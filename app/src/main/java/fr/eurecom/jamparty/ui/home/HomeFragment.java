7c53c6ee5516a4ff7d28c61770745e9f11663fa2
GIUSEPPE ORLANDO s308465 <s308465@studenti.polito.it>
2023-11-28 16:39:55 +0100

    Added button functionality for player buttons

--- app/src/main/java/fr/eurecom/jamparty/ui/home/HomeFragment.java
+++ app/src/main/java/fr/eurecom/jamparty/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,8 +26,30 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //final TextView textView = binding.textHome;
-        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        ImageButton playButton = binding.playButton;
+        ImageButton backButton = binding.backButton;
+        ImageButton nextButton = binding.nextButton;
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Clicked play!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Clicked next!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Clicked back!", Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 
