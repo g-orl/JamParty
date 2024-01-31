20134e5a2dc4af061a8025853cbd8d889fcd217e
AndrewL73 <leoneandrea7300@gmail.com>
2024-01-31 14:43:27 +0100

    fix: dislike button

--- app/src/main/java/fr/eurecom/jamparty/objects/adapters/SuggestionAdapter.java
+++ app/src/main/java/fr/eurecom/jamparty/objects/adapters/SuggestionAdapter.java
@@ -1,51 +1,28 @@
 package fr.eurecom.jamparty.objects.adapters;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
 
 import fr.eurecom.jamparty.R;
-import fr.eurecom.jamparty.SpotifyApiPostTask;
 import fr.eurecom.jamparty.objects.Room;
-import fr.eurecom.jamparty.objects.Song;
 import fr.eurecom.jamparty.objects.Suggestion;
 import fr.eurecom.jamparty.ui.fragments.RoomFragment;
-import fr.eurecom.jamparty.ui.home.HomeFragment;
 
 
 public class SuggestionAdapter extends RecyclerView.Adapter<SuggestionAdapter.ViewHolder> {
+
 
     private RoomFragment caller;
     private Room room;
@@ -78,33 +55,12 @@ public class SuggestionAdapter extends RecyclerView.Adapter<SuggestionAdapter.Vi
     @Override
     public SuggestionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.memory_song, parent, false);
-        ViewHolder ret = new ViewHolder(view);
-        /*
-        ret.popupView = caller.getLayoutInflater().inflate(R.layout.suggestion_popup, null);
-        ret.popupView.findViewById(R.id.dislike_button).setOnClickListener(new View.OnClickListener() {
-            private boolean isDisliked = false;
-            @Override
-            public void onClick(View v) {
-                // dislike the song
-                if (isDisliked) {
-                    if(ret.suggestion != null)
-                        ret.suggestion.upvote();
-                    ret.popupView.findViewById(R.id.dislike_button).setBackground(caller.getContext().getDrawable(R.drawable.thumb_nobg));
-                } else {
-                    if(ret.suggestion != null)
-                        ret.suggestion.downvote();
-                    ret.popupView.findViewById(R.id.dislike_button).setBackground(caller.getContext().getDrawable(R.drawable.thumb_red_nobg));
-                }
-                isDisliked = !isDisliked;
-                room.pushSongsToDb();
-            }
-        });
-        */
-        return ret;
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SuggestionAdapter.ViewHolder holder, int position) {
+        Log.i("LIKE pos", String.valueOf(position));
         Suggestion suggestion = suggestions.get(position);
         if(suggestion == null) return;
 
