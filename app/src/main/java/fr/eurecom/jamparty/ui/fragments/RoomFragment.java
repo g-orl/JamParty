8f3c3a3f1bfb7908bb2741a250ffa9539e35dc52
AndrewL73 <leoneandrea7300@gmail.com>
2024-01-31 17:51:23 +0100

    fix: queue

--- app/src/main/java/fr/eurecom/jamparty/ui/fragments/RoomFragment.java
+++ app/src/main/java/fr/eurecom/jamparty/ui/fragments/RoomFragment.java
@@ -105,18 +105,16 @@ public class RoomFragment extends Fragment {
 
         roomRef.child("queue").addChildEventListener(new ChildEventListener() {
             @Override public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Suggestion suggestion = snapshot.getValue(Suggestion.class);
-                    Log.i("CHILD INDEX", "Added " + suggestion.getName() + " | size = "+room.getQueue().size());
-                    if (room.findSuggestion(suggestion) == null)
-                        room.addToQueue(suggestion);
-                    suggestionAdapter.notifyDataSetChanged();
-                    // need to remove this suggestion from my suggestion queue
-                    if (RoomUserManager.userOwnsRoom(MainActivity.getUser(), room)) {
-                        Log.i("TIMER", "Starting timer for "+suggestion.getName()+" | "+suggestion.getAuthor());
-                        SongTimer task = new SongTimer(room, room.findSuggestion(suggestion));
-                        new Timer().schedule(task, 15000);
-                    }
+                Suggestion suggestion = dataSnapshot.getValue(Suggestion.class);
+                Log.i("CHILD INDEX", "Added " + suggestion.getName() + " | size = "+room.getQueue().size());
+                if (room.findSuggestion(suggestion) == null)
+                    room.addToQueue(suggestion);
+                suggestionAdapter.notifyDataSetChanged();
+                // need to remove this suggestion from my suggestion queue
+                if (RoomUserManager.userOwnsRoom(MainActivity.getUser(), room)) {
+                    Log.i("TIMER", "Starting timer for "+suggestion.getName()+" | "+suggestion.getAuthor());
+                    SongTimer task = new SongTimer(room, room.findSuggestion(suggestion));
+                    new Timer().schedule(task, 15000);
                 }
             }
             @Override
